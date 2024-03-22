aa61d433c0dde02916c499855544b91046a946ef
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-03-22 21:14:35 +0100

    Update BPM.bat
    I pretty much added package installing (huge)

--- BPM.bat
+++ BPM.bat
@@ -4,18 +4,23 @@ setlocal enabledelayedexpansion
 chcp 65001 > nul 2>&1
 for /f %%a in ('echo prompt $E^| cmd') do set "\e=%%a"
 
-if not exist "%~dp0\packages"
+if not exist "%~dp0\packages" md "%~dp0\packages"
 
 if "%~1"=="" goto --help
 if "%~1"=="/?" goto --help
 for %%a in (
-	"-$ --help" "-G --get" "-S --search" "-L --list" "-U --update" "-D --uninstall" "-V --version"
+	"-$ --help" "-I --install" "-S --search" "-L --list" "-U --update" "-D --uninstall" "-V --version"
 ) do for /f "tokens=1,2" %%b in (%%a) do (
 	set "option=%%~b"
-	if "%~1"=="!option:$=?!" call :%%~c %2 %3
-	if "%~1"=="%%~c" call :%*
+	if /I "%~1"=="!option:$=?!" (
+		call :%%~c %2 %3 %4 %5 %6 %7 %8 %9 && REM This looks stupid
+		exit /b !errorlevel!
+	) else if /I "%~1"=="%%~c" (
+		call :%*
+		exit /b !errorlevel!
+	)
 )
-exit /b
+exit /b -1
 :parse-db
 set line=0
 set mode=#
@@ -29,18 +34,18 @@ for /f "tokens=1* delims=;" %%a in ('type "%~dp0\database.txt"') do (
 	)
 	if "!token!"=="[\Downloads]" set mode=
 	if "!mode!"=="Downloads" for %%x in ("!item!") do (
-		set item.[!item!].downloads=!item.[%%~x].downloads! "!token!=%%~b"
+		set item.[!item!].downloads=!item.[%%~x].downloads! "!token!"
+		set "item.[!item!].download.[!token!]=%%~b"
 	)
 	if "!token!"=="[Downloads]" set mode=Downloads
 	if "!mode!"=="Item" (
 		set "item.[!item!].%%~a=%%~b"
-	) else (
-		if "!token:~0,1!"==":" (
-			set mode=Item
-			set "item=!token:~1!"
-			set item.[!item!]=Hello this is defined qwq
-			set all_items=!all_items! "!token:~1!"
-		)
+	)
+	if "!token:~0,1!"==":" (
+		set mode=Item
+		set "item=!token:~1!"
+		set item.[!item!]=Hello this is defined qwq
+		set all_items=!all_items! "!token:~1!"
 	)
 )
 exit /b
@@ -53,11 +58,60 @@ if exist "%~dp0database.txt" exit /b
 :db-err
 echo(Something went wrong. Check your internet connection.
 exit /b 1
-:--get
+:--install
 call :get-db
 call :parse-db
-if not defined item.[%1]
-exit /b
+set return=0
+for %%a in (%*) do for /f "tokens=1,2 delims=:" %%i in ("%%~a") do if defined item.[%%~i] (
+	if not exist "%~dp0BPM-temp" (
+		md "%~dp0BPM-temp"
+	) else (
+		echo(%\e%[38;2;255;255;255mIt seems like other packages are installing,
+		echo(or the installation was cancelled unexpectedly.
+		echo(Are you sure you want to continue installing?
+		choice
+		if "!errorlevel!" neq "1" exit /b
+		del /Q "%~dp0BPM-temp"
+		if not exist "%~dp0BPM-temp" md "%~dp0BPM-temp" && REM Just in case if somebody decided to make a file with the name "BPM-temp"
+	)
+	set link=
+	set "packageid=%%~i"
+	if "%%~j"=="" (
+		set "link=!item.[%%~i].defaultDownload!"
+	) else (
+		if defined item.[%%~i].download.[%%~j] (
+			set "link=!item.[%%~i].download.[%%~j]!"
+		) else (
+			echo(%\e%[38;2;255;127;127mThe package version "%%~j" for "%%~i" was not found.%\e%[E%\e%[38;2;255;255;127mIf you're looking for a specific version of a package, try using '%\e%[38;2;0;255;255mBPM --info ^<package name^>%\e%[38;2;255;255;127m'.%\e%[38;2;255;255;255m
+			set return=1
+		)
+	)
+	
+	for /f "tokens=1,2 delims=;" %%x in ("!link!") do (REM The link format is actually "<link>;<package type>". This seperates it.
+		set "link=%%~x"
+		set "packagetype=%%~y"
+	)
+	if defined packagetype (
+		if /I "!packagetype!"=="zip" (
+			call curl -# -o "%~dp0BPM-temp\package.zip" "!link!"
+			if not exist "%~dp0packages\!packageid!" md "%~dp0packages\!packageid!"
+			set "returndir=%cd%"
+			cd "%~dp0packages\!packageid!\"
+			tar -xf "%~dp0BPM-temp\package.zip"
+			if exist "install.bat" call install.bat "!packageid!"
+			cd "!returndir!"
+		) else if /I "!packagetype!"=="bat" (
+			call curl -# -o "%~dp0BPM-temp\package.bat" "!link!"
+		) else if /I "!packagetype!"=="cmd" (
+			call curl -# -o "%~dp0BPM-temp\package.cmd" "!link!"
+		) else REM   \/
+	) else REM Todo: Add handeling for invalid package types
+) else (
+	echo(%\e%[38;2;255;127;127mThe package "%%~a" was not found.%\e%[E%\e%[38;2;255;255;127mMake sure you didn't use quotes. The IDs are also case sensitive.%\e%[EIf you're looking for something, try using '%\e%[38;2;0;255;255mBPM --search%\e%[38;2;255;255;127m'.%\e%[38;2;255;255;255m
+	set return=1
+)
+if exist "%~dp0BPM-temp" rd /s /q "%~dp0BPM-temp"
+exit /b %return%
 :--search
 call :get-db
 call :parse-db
@@ -117,7 +171,7 @@ for %%a in (
 	"    -$|--help| |Displays the help prompt."
 	"    -V|--version|[<identifier>]|Displays the installed version of a specified package."
 	"      |         |              |If no ID is specified, displays all (Including BPM)."
-	"    -G|--get|<identifier>|Install a package by its identifier."
+	"    -I|--install|<identifier>|Install a package by its identifier."
 	"    -S|--search|<keywords>|Searches for packages by keywords."
 	"    -L|--list|[<keywords>]|Lists installed packages."
 	"    -U|--update|[<identifier>]|Updates a package. If no ID is specified, updates all."
